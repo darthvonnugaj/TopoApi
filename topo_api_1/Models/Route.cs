16c62d6c57f1a03c567d79dac31db310a51e05c1
darthvonnugaj <75%uk9gdkeGIT>
2018-04-18 17:10:37 +0200

    Init

---
+++ topo_api_1/Models/Route.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace topo_api_1.Models
+{
+    public class Route
+    {
+        public enum RouteType
+        {
+            Sport,
+            Boulder,
+            Trad,
+            MountainSummer,
+            MountainWinter,
+            Icefall
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public RouteType Type { get; set; }
+        public string Grade { get; set; }
+        public string Img { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+}
