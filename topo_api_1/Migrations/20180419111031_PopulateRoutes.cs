f916732466e19e80582e25762d7c56e7556aab40
darthvonnugaj@gmail.com <75%uk9gdkeGIT>
2018-04-19 13:47:58 +0200

    Migration from sql

---
+++ topo_api_1/Migrations/20180419111031_PopulateRoutes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.ApplicationInsights.AspNetCore;
+
+namespace topo_api_1.Migrations
+{
+    public partial class PopulateRoutes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Migrations/MockRoute.sql");
+            string sql = File.ReadAllText(sqlFile);
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
